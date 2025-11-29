269f36d5e459cbdbfb97716f982a7e9c0a4bb8ed
신관규 <soundbar91@gmail.com>
2025-11-29 21:10:20 +0900

    fix: 공지사항 엔티티 클래스 Entity 어노테이션 추가

--- src/main/java/com/example/konect/notice/model/Notice.java
+++ src/main/java/com/example/konect/notice/model/Notice.java
@@ -6,6 +6,7 @@ import static lombok.AccessLevel.PROTECTED;
 import com.example.konect.common.model.BaseEntity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -14,6 +15,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
 @Table(name = "notice")
 @NoArgsConstructor(access = PROTECTED)
