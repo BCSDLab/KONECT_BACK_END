9b6b78c6af697d504a6952bfdb655291d24c0354
신관규 <soundbar91@gmail.com>
2025-11-29 21:03:01 +0900

    feat: 공지사항 조회 API 명세 작성

---
+++ src/main/java/com/example/konect/notice/controller/NoticeApi.java
@@ -0,0 +1,23 @@
+package com.example.konect.notice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.konect.notice.dto.NoticesResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "(Normal) Notice: 공지사항", description = "공지사항 API")
+@RequestMapping("/notice")
+public interface NoticeApi {
+
+    @Operation(summary = "페이지 네이션으로 공지사항을 조회한다.")
+    @GetMapping
+    ResponseEntity<NoticesResponse> getNotices(
+        @RequestParam(name = "page", defaultValue = "1") Integer page,
+        @RequestParam(name = "limit", defaultValue = "10", required = false) Integer limit
+    );
+}
---
+++ src/main/java/com/example/konect/notice/dto/NoticesResponse.java
@@ -0,0 +1,52 @@
+package com.example.konect.notice.dto;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.konect.notice.model.Notice;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record NoticesResponse(
+    @Schema(description = "조건에 해당하는 공지사항 수", example = "10", requiredMode = REQUIRED)
+    Long totalCount,
+
+    @Schema(description = "현재 페이지에서 조회된 공지사항 수", example = "5", requiredMode = REQUIRED)
+    Integer currentCount,
+
+    @Schema(description = "최대 페이지", example = "2", requiredMode = REQUIRED)
+    Integer totalPage,
+
+    @Schema(description = "현재 페이지", example = "1", requiredMode = REQUIRED)
+    Integer currentPage,
+
+    @Schema(description = "공지사항 리스트", requiredMode = REQUIRED)
+    List<InnerNoticeResponse> notices
+) {
+    public record InnerNoticeResponse(
+        @Schema(description = "공지사항 고유 id", example = "1", requiredMode = REQUIRED)
+        Integer id,
+
+        @Schema(description = "공지사항 제목", example = "동아리 박람회 참가 신청 마감 안내", requiredMode = REQUIRED)
+        String title
+    ) {
+        public static InnerNoticeResponse of(Notice notice) {
+            return new InnerNoticeResponse(notice.getId(), notice.getTitle());
+        }
+    }
+
+    public static NoticesResponse of(Page<Notice> page) {
+        return new NoticesResponse(
+            page.getTotalElements(),
+            page.getNumberOfElements(),
+            page.getTotalPages(),
+            page.getNumber() + 1,
+            page.stream()
+                .map(NoticesResponse.InnerNoticeResponse::of)
+                .toList()
+        );
+    }
+}
