f4e8e8117962b7c56ef17335b31d3d127bdd9bfc
Donghun <2dh2@naver.com>
2025-11-30 23:45:43 +0900

    feat: 동아리 임원진 엔티티 생성

---
+++ src/main/java/com/example/konect/club/model/ClubExecutive.java
@@ -0,0 +1,54 @@
+package com.example.konect.club.model;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.example.konect.common.model.BaseEntity;
+import com.example.konect.user.model.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "club_executive")
+@NoArgsConstructor(access = PROTECTED)
+public class ClubExecutive extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
+    private Integer id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "is_representative", nullable = false)
+    private Boolean isRepresentative;
+
+    @Builder
+    private ClubExecutive(Integer id, Club club, User user, String name, Boolean isRepresentative) {
+        this.id = id;
+        this.club = club;
+        this.user = user;
+        this.name = name;
+        this.isRepresentative = isRepresentative;
+    }
+}
