3855eda00f35c07bd53c8e05f6768f305030d083
이동훈 <2dh2@naver.com>
2025-12-01 19:05:54 +0900

    fix: Repository 클래스에 잘못된 제네릭 타입 수정

--- src/main/java/com/example/konect/club/repository/ClubExecutiveRepository.java
+++ src/main/java/com/example/konect/club/repository/ClubExecutiveRepository.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 import com.example.konect.club.model.ClubExecutive;
+import com.example.konect.club.model.ClubMemberId;
 
-public interface ClubExecutiveRepository extends Repository<ClubExecutive, Integer> {
+public interface ClubExecutiveRepository extends Repository<ClubExecutive, ClubMemberId> {
 
     Optional<ClubExecutive> findByClubIdAndIsRepresentative(Integer clubId, Boolean isRepresentative);
 
--- src/main/java/com/example/konect/club/repository/ClubMemberRepository.java
+++ src/main/java/com/example/konect/club/repository/ClubMemberRepository.java
@@ -3,8 +3,9 @@ package com.example.konect.club.repository;
 import org.springframework.data.repository.Repository;
 
 import com.example.konect.club.model.ClubMember;
+import com.example.konect.club.model.ClubMemberId;
 
-public interface ClubMemberRepository extends Repository<ClubMember, Integer> {
+public interface ClubMemberRepository extends Repository<ClubMember, ClubMemberId> {
 
     long countByClubId(Integer clubId);
 }
