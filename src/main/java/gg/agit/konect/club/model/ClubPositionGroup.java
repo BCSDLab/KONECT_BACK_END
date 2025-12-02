579040e395f4c42632373e8b53181e33983d8418
신관규 <soundbar91@gmail.com>
2025-12-02 22:43:23 +0900

    feat: ClubPositionGroup 엔티티 클래스 추가

---
+++ src/main/java/gg/agit/konect/club/model/ClubPositionGroup.java
@@ -0,0 +1,35 @@
+package gg.agit.konect.club.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import gg.agit.konect.common.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "club_position_group")
+@NoArgsConstructor(access = PROTECTED)
+public class ClubPositionGroup extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false, unique = true)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Builder
+    private ClubPositionGroup(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
