77da3ca1ca9c52b5156d78e95947f81d39c6b635
이동훈 <2dh2@naver.com>
2025-12-05 14:00:43 +0900

    feat: OAuth 로그인 및 시큐리티 필터 체인 설정

---
+++ src/main/java/gg/agit/konect/security/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package gg.agit.konect.security.config;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import gg.agit.konect.security.oauth.service.GoogleOAuthServiceImpl;
+import gg.agit.konect.security.oauth.service.SocialOAuthService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private Map<String, SocialOAuthService> oAuthServices;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, GoogleOAuthServiceImpl googleOAuthServiceImpl) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            ).oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfo -> userInfo
+                    .userService(userRequest -> {
+                        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+                        return oAuthServices.get(registrationId).loadUser(userRequest);
+                    })
+                )
+            );
+
+        return http.build();
+    }
+}
