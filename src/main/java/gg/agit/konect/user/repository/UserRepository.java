0e850dfd6c60339b890af6b1fdfb7aeefa7bfed3
이동훈 <64298482+dh2906@users.noreply.github.com>
2025-12-07 17:34:57 +0900

    feat: 세션 발급, 로그인 성공 핸들러 구현 (#33)
    * chore: 부모 브랜치 변경 사항 리베이스로 반영
    
    * build: jwt 라이브러리 의존성 추가
    
    * feat: 회원가입 여부 컬럼 추가
    
    * feat: JWT 발급기 추가
    
    * fix: 회원가입을 하지 않았을 시 가입 관련 응답 반환하도록 수정
    
    * fix: 토큰 발급기 사용하도록 수정
    
    * fix: 임시 회원가입으로 인한 이름, 전화번호, 학번 컬럼 Not Null 제거
    
    * fix: 임시 회원가입시 저장하던 값 Null 허용으로 인해 제거
    
    * chore: 코드 포맷팅
    
    * fix: JWT 발급기 제거
    
    * fix: jwt -> 세션으로 인증 수단 변경
    
    * chore: 불필요한 라이브러리, 응답 코드 제거
    
    * feat: 미가입 유저에 대한 테이블 생성
    
    * fix: 테이블 제약조건 수정
    
    * feat: 미가입 회원 엔티티 생성
    
    * feat: User 엔티티에 다중 Unique 제약조건 추가
    
    * feat: User 조회 조건 추가
    
    * feat: 미가입 유저 Repository 생성
    
    * feat: 핸들링에서 미가입 유저에 대한 처리 방식 수정
    
    * fix: DDL 오류 수정
    
    * fix: User 엔티티 가입 여부 필드 제거
    
    * feat: 로그인에 따른 응답 DTO 추가

--- src/main/java/gg/agit/konect/user/repository/UserRepository.java
+++ src/main/java/gg/agit/konect/user/repository/UserRepository.java
@@ -4,11 +4,19 @@ import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
+import gg.agit.konect.global.code.ApiResponseCode;
+import gg.agit.konect.global.exception.CustomException;
+import gg.agit.konect.security.enums.Provider;
 import gg.agit.konect.user.model.User;
 
 public interface UserRepository extends Repository<User, Integer> {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndProvider(String email, Provider provider);
+
+    default User getByEmailAndProvider(String email, Provider provider) {
+        return findByEmailAndProvider(email, provider).orElseThrow(() ->
+            CustomException.of(ApiResponseCode.NOT_FOUND_USER));
+    }
 
     User save(User user);
 }
