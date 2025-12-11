daa955987531ab18d63674865f76c3e73ea11a1a
soundbar91 <soundbar91@jinhakapply.com>
2025-12-11 12:07:38 +0900

    feat: CouncilCreateRequest instagramUrl 유효성 검증 추가

--- src/main/java/gg/agit/konect/council/dto/CouncilCreateRequest.java
+++ src/main/java/gg/agit/konect/council/dto/CouncilCreateRequest.java
@@ -3,10 +3,9 @@ package gg.agit.konect.council.dto;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import gg.agit.konect.council.model.Council;
-import gg.agit.konect.council.model.CouncilSocialMedia;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CouncilCreateRequest(
@@ -34,28 +33,12 @@ public record CouncilCreateRequest(
     @Schema(description = "총동아리연합회 운영 시간", example = "평일 09:00 ~ 18:00", requiredMode = REQUIRED)
     String operatingHour,
 
+    @NotEmpty(message = "총동아리연합회 인스타 주소는 필수 입력입니다.")
+    @Size(max = 255, message = "총동아리연합회 인스타 주소는 최대 255자 입니다.")
+    @Pattern(regexp = "^https?://(www\\.)?instagram\\.com/[a-zA-Z0-9._]+/?$", message = "올바른 인스타그램 URL 형식이 아닙니다.")
     @Schema(description = "총동아리연합회 인스타 주소", example = "https://www.instagram.com/koreatech_council", requiredMode = REQUIRED)
     String instagramUrl
 ) {
-    public record InnerSocialMedia(
-        @NotBlank(message = "플랫폼 이름은 필수 입력값입니다.")
-        @Size(max = 255, message = "플랫폼 이름은 최대 255자 입니다.")
-        @Schema(description = "플랫폼 이름", example = "인스타그램", requiredMode = REQUIRED)
-        String name,
-
-        @NotBlank(message = "플랫폼 url은 필수 입력값입니다.")
-        @Size(max = 255, message = "플랫폼 url은 최대 255자 입니다.")
-        @Schema(description = "플랫폼 url", example = "https://www.instagram.com/koreatech_council", requiredMode = REQUIRED)
-        String url
-    ) {
-        public CouncilSocialMedia toEntity(Council council) {
-            return CouncilSocialMedia.builder()
-                .council(council)
-                .platformName(name)
-                .url(url)
-                .build();
-        }
-    }
 
     public Council toEntity() {
         return Council.builder()
