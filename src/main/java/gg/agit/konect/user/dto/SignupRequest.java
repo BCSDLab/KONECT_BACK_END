6422c15eff1984e92759bfad0c0e14e773672505
이동훈 <2dh2@naver.com>
2025-12-12 22:30:50 +0900

    feat: 회원가입 요청 값에 마케팅 동의 여부 추가

--- src/main/java/gg/agit/konect/user/dto/SignupRequest.java
+++ src/main/java/gg/agit/konect/user/dto/SignupRequest.java
@@ -4,6 +4,7 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -21,6 +22,10 @@ public record SignupRequest(
     @NotEmpty(message = "학번은 필수 입력입니다.")
     @Size(max = 20, message = "학번은 최대 20자 입니다.")
     @Schema(description = "회원 학번", example = "20250001", requiredMode = REQUIRED)
-    String studentNumber
+    String studentNumber,
+
+    @NotNull(message = "마케팅 동의 여부는 필수입니다.")
+    @Schema(description = "마케팅 수신 동의 여부", example = "true", requiredMode = REQUIRED)
+    Boolean marketingAgreement
 ){
 }
