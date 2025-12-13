4d775d7e53dd4f8bebbb6d593be4002df37a3c54
이동훈 <2dh2@naver.com>
2025-12-13 22:49:22 +0900

    chore: 미사용 메소드 제거

--- src/main/java/gg/agit/konect/user/repository/UserRepository.java
+++ src/main/java/gg/agit/konect/user/repository/UserRepository.java
@@ -13,11 +13,6 @@ public interface UserRepository extends Repository<User, Integer> {
 
     Optional<User> findByEmailAndProvider(String email, Provider provider);
 
-    default User getByEmailAndProvider(String email, Provider provider) {
-        return findByEmailAndProvider(email, provider).orElseThrow(() ->
-            CustomException.of(ApiResponseCode.NOT_FOUND_USER));
-    }
-
     Optional<User> findById(Integer id);
 
     default User getById(Integer id) {
