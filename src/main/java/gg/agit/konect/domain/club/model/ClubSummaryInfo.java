53d3f59d48883ac5927457c255be7de39b5a4f79
신관규 <soundbar91@gmail.com>
2025-12-16 22:08:29 +0900

    feat: GET /clubs 동아리 모집 상태 응답값 추가 (#57)
    * feat: 동아리 리스트 조회 API 모집 여부 응답값 추
    
    * docs: GET /clubs 설명 추가
    
    * fix: users 목데이터 수정

--- src/main/java/gg/agit/konect/domain/club/model/ClubSummaryInfo.java
+++ src/main/java/gg/agit/konect/domain/club/model/ClubSummaryInfo.java
@@ -2,12 +2,15 @@ package gg.agit.konect.domain.club.model;
 
 import java.util.List;
 
+import gg.agit.konect.domain.club.enums.RecruitmentStatus;
+
 public record ClubSummaryInfo(
     Integer id,
     String name,
     String imageUrl,
     String categoryName,
     String description,
+    RecruitmentStatus status,
     List<String> tags
 ) {
 
