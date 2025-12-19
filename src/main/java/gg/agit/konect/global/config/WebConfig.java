e60dfa40d2bd8f3d85aa79b9df4a71ea06c1d328
이동훈 <2dh2@naver.com>
2025-12-19 09:46:39 +0900

    feat: 인터셉터 등록

--- src/main/java/gg/agit/konect/global/config/WebConfig.java
+++ src/main/java/gg/agit/konect/global/config/WebConfig.java
@@ -1,12 +1,14 @@
 package gg.agit.konect.global.config;
 
-import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import gg.agit.konect.global.auth.interceptor.LoginCheckInterceptor;
 import gg.agit.konect.global.auth.resolver.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
     private final CorsProperties corsProperties;
+    private final LoginCheckInterceptor loginCheckInterceptor;
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
@@ -30,5 +33,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor)
+            .addPathPatterns("/**");
     }
 }
