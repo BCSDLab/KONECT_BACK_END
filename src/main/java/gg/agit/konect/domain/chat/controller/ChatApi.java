d69ba9619c15c15d1e00f70d29d6cb32ac443b63
신관규 <soundbar91@gmail.com>
2025-12-19 22:59:32 +0900

    feat: GET /chats/room/{chatRoomId}/messages 명세 작성

--- src/main/java/gg/agit/konect/domain/chat/controller/ChatApi.java
+++ src/main/java/gg/agit/konect/domain/chat/controller/ChatApi.java
@@ -3,14 +3,19 @@ package gg.agit.konect.domain.chat.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import gg.agit.konect.domain.chat.dto.ChatMessagesResponse;
 import gg.agit.konect.domain.chat.dto.ChatRoomsResponse;
+import gg.agit.konect.domain.chat.dto.ChatMessageResponse;
+import gg.agit.konect.domain.chat.dto.ChatMessageSendRequest;
 import gg.agit.konect.global.auth.annotation.UserId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @Tag(name = "(Normal) Chat: 채팅", description = "채팅 API")
 @RequestMapping("/chats")
@@ -27,6 +32,14 @@ public interface ChatApi {
         @RequestParam(name = "page", defaultValue = "1") Integer page,
         @RequestParam(name = "limit", defaultValue = "20", required = false) Integer limit,
         @PathVariable(value = "chatRoomId") Integer chatRoomId,
+        @UserId Integer userId
+    );
+
+    @Operation(summary = "메시지를 전송한다.")
+    @PostMapping("/rooms/{chatRoomId}/messages")
+    ResponseEntity<ChatMessageResponse> sendMessage(
+        @PathVariable(value = "chatRoomId") Integer chatRoomId,
+        @Valid @RequestBody ChatMessageSendRequest request,
         @UserId Integer userId
     );
 }
