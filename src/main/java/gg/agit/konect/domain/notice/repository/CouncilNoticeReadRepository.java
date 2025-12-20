509bb08c62330e659f4f78464e8553acac0aa301
신관규 <soundbar91@gmail.com>
2025-12-21 00:09:24 +0900

    feat: GET /users/me 응답값 추가 (#71)
    * feat: UserInfoResponse 응답값 추가
    
    * feat: UserInfoResponse 응답값 추가로 인한 로직 구현

--- src/main/java/gg/agit/konect/domain/notice/repository/CouncilNoticeReadRepository.java
+++ src/main/java/gg/agit/konect/domain/notice/repository/CouncilNoticeReadRepository.java
@@ -2,7 +2,9 @@ package gg.agit.konect.domain.notice.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import gg.agit.konect.domain.notice.model.CouncilNoticeReadHistory;
 
@@ -13,4 +15,15 @@ public interface CouncilNoticeReadRepository extends Repository<CouncilNoticeRea
     List<CouncilNoticeReadHistory> findByUserIdAndCouncilNoticeIdIn(Integer userId, List<Integer> councilNoticeIds);
 
     void save(CouncilNoticeReadHistory councilNoticeReadHistory);
+
+    @Query("""
+        SELECT COUNT(cn)
+        FROM CouncilNotice cn
+        WHERE NOT EXISTS (
+                SELECT 1
+                FROM CouncilNoticeReadHistory cnrh
+                WHERE cnrh.councilNotice = cn AND cnrh.user.id = :userId
+                )
+        """)
+    Long countUnreadNoticesByUserId(@Param("userId") Integer userId);
 }
--- src/main/java/gg/agit/konect/domain/user/dto/UserInfoResponse.java
+++ src/main/java/gg/agit/konect/domain/user/dto/UserInfoResponse.java
@@ -3,6 +3,10 @@ package gg.agit.konect.domain.user.dto;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
+import java.time.LocalTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import gg.agit.konect.domain.user.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -20,16 +24,29 @@ public record UserInfoResponse(
     String phoneNumber,
 
     @Schema(description = "사용자 프로필 이미지 링크", example = "https://stage-static.koreatech.in/konect/User_02.png",requiredMode = REQUIRED)
-    String imageUrl
+    String imageUrl,
+
+    @Schema(description = "가입 동아리 개수", example = "1", requiredMode = REQUIRED)
+    Integer joinedClubCount,
+
+    @Schema(description = "순공 시간", example = "13:13", requiredMode = REQUIRED)
+    @JsonFormat(pattern = "HH:mm")
+    LocalTime studyTime,
+
+    @Schema(description = "읽지 않은 총 동아리 연합회 공지", example = "1", requiredMode = REQUIRED)
+    Long unreadCouncilNoticeCount
 ) {
 
-    public static UserInfoResponse from(User user) {
+    public static UserInfoResponse from(User user, Integer joinedClubCount, Long unreadCouncilNoticeCount) {
         return new UserInfoResponse(
             user.getName(),
             user.getUniversity().getKoreanName(),
             user.getStudentNumber(),
             user.getPhoneNumber(),
-            user.getImageUrl()
+            user.getImageUrl(),
+            joinedClubCount,
+            LocalTime.of(0, 0, 0),
+            unreadCouncilNoticeCount
         );
     }
 }
