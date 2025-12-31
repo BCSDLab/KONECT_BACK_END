f5661d215e7a1c9b550f7935cf9f6e42a48210e4
soundbar91 <soundbar91@jinhakapply.com>
2025-12-31 14:26:17 +0900

    fix: UniversitySchedule 클래스 추상 클래스 상속 받도록 수정

--- src/main/java/gg/agit/konect/domain/schedule/model/UniversitySchedule.java
+++ src/main/java/gg/agit/konect/domain/schedule/model/UniversitySchedule.java
@@ -1,23 +1,16 @@
 package gg.agit.konect.domain.schedule.model;
 
 import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 import gg.agit.konect.domain.university.model.University;
-import gg.agit.konect.global.model.BaseEntity;
-import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,49 +18,23 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Table(name = "university_schedule")
+@DiscriminatorValue("UNIVERSITY")
 @NoArgsConstructor(access = PROTECTED)
-public class UniversitySchedule extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false, unique = true)
-    private Integer id;
+public class UniversitySchedule extends Schedule {
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "university_id", nullable = false)
     private University university;
 
-    @NotNull
-    @Column(name = "title", nullable = false)
-    private String title;
-
-    @NotNull
-    @Column(name = "started_at", nullable = false)
-    private LocalDateTime startedAt;
-
-    @NotNull
-    @Column(name = "ended_at", nullable = false)
-    private LocalDateTime endedAt;
-
     @Builder
     private UniversitySchedule(
         Integer id,
-        University university,
         String title,
         LocalDateTime startedAt,
-        LocalDateTime endedAt
+        LocalDateTime endedAt,
+        University university
     ) {
-        this.id = id;
+        super(id, title, startedAt, endedAt);
         this.university = university;
-        this.title = title;
-        this.startedAt = startedAt;
-        this.endedAt = endedAt;
-    }
-
-    public Integer calculateDDay(LocalDate today) {
-        if (today.isBefore(this.startedAt.toLocalDate())) {
-            return (int)ChronoUnit.DAYS.between(today, this.startedAt.toLocalDate());
-        }
-        return null;
     }
 }
