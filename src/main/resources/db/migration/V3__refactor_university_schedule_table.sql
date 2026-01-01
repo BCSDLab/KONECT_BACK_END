d983d35a06ce9f8c1933cc836e024255975e0196
신관규 <soundbar91@gmail.com>
2026-01-01 23:05:30 +0900

    Revert "fix: V3 flyway 수정"
    This reverts commit fdfc91809470c35960a38c33088d4b89be7a0100.

--- src/main/resources/db/migration/V3__refactor_university_schedule_table.sql
+++ src/main/resources/db/migration/V3__refactor_university_schedule_table.sql
@@ -1,7 +1,7 @@
 ALTER TABLE university_schedule
-    DROP COLUMN IF EXISTS title,
-    DROP COLUMN IF EXISTS started_at,
-    DROP COLUMN IF EXISTS ended_at;
+    DROP COLUMN title,
+    DROP COLUMN started_at,
+    DROP COLUMN ended_at;
 
 ALTER TABLE university_schedule
     MODIFY COLUMN id INT NOT NULL;
