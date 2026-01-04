1ca730a3b24dd485434224477913eb89a1d014d6
신관규 <soundbar91@gmail.com>
2026-01-04 17:32:02 +0900

    refactor: 불필요한 WithdrawnUser 아카이브 기능 제거

--- src/main/java/gg/agit/konect/global/config/SchedulerConfig.java
+++
@@ -1,9 +0,0 @@
-package gg.agit.konect.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulerConfig {
-}
