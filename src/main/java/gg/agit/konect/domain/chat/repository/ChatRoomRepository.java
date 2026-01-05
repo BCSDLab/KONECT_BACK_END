cea36346228c489519e808676154f0638e91ee37
신관규 <soundbar91@gmail.com>
2026-01-05 22:39:05 +0900

    chore: 미사용 import 삭제

--- src/main/java/gg/agit/konect/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/gg/agit/konect/domain/chat/repository/ChatRoomRepository.java
@@ -3,7 +3,6 @@ package gg.agit.konect.domain.chat.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
