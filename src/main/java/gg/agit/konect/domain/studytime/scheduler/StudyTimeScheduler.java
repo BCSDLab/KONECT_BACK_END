6cbcc2f56f9f11c9e307c282ff383c1e2d71e4da
soundbar91 <soundbar91@jinhakapply.com>
2026-01-07 15:22:10 +0900

    feat: 동아리 공부 시간 랭킹 업데이트 스케줄러 등록

--- src/main/java/gg/agit/konect/domain/studytime/scheduler/StudyTimeScheduler.java
+++ src/main/java/gg/agit/konect/domain/studytime/scheduler/StudyTimeScheduler.java
@@ -1,4 +1,27 @@
 package gg.agit.konect.domain.studytime.scheduler;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import gg.agit.konect.domain.studytime.service.StudyTimeSchedulerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class StudyTimeScheduler {
+
+    private final StudyTimeSchedulerService studyTimeSchedulerService;
+
+    @Scheduled(fixedDelay = 5, timeUnit = MINUTES)
+    public void clubStudyTimeRankingUpdate() {
+        try {
+            studyTimeSchedulerService.updateClubStudyTimeRanking();
+        } catch (Exception e) {
+            log.error("동아리 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
+        }
+    }
 }
