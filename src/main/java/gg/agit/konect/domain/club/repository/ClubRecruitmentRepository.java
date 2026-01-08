5d156fd996f75d282edc4f16ed73693721295da6
신관규 <soundbar91@gmail.com>
2026-01-08 22:01:17 +0900

    fix: ClubRecruitmentResponse 이미지 응답값 수정

--- src/main/java/gg/agit/konect/domain/club/repository/ClubRecruitmentRepository.java
+++ src/main/java/gg/agit/konect/domain/club/repository/ClubRecruitmentRepository.java
@@ -4,14 +4,23 @@ import static gg.agit.konect.global.code.ApiResponseCode.NOT_FOUND_CLUB_RECRUITM
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import gg.agit.konect.domain.club.model.ClubRecruitment;
 import gg.agit.konect.global.exception.CustomException;
 
 public interface ClubRecruitmentRepository extends Repository<ClubRecruitment, Integer> {
 
-    Optional<ClubRecruitment> findByClubId(Integer clubId);
+    @Query("""
+        SELECT c
+        FROM ClubRecruitment c
+        LEFT JOIN FETCH c.images i
+        WHERE c.club.id = :clubId
+        ORDER BY i.displayOrder ASC
+        """)
+    Optional<ClubRecruitment> findByClubId(@Param("clubId") Integer clubId);
 
     default ClubRecruitment getByClubId(Integer clubId) {
         return findByClubId(clubId)
