a780ba94f28125e3266c34bd4bbba655ccacec2b
이동훈 <2dh2@naver.com>
2026-01-11 16:54:44 +0900

    fix: 로그인해야 API 사용 가능 하도록 수정

--- src/main/java/gg/agit/konect/domain/bank/controller/BankApi.java
+++ src/main/java/gg/agit/konect/domain/bank/controller/BankApi.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import gg.agit.konect.domain.bank.dto.BanksResponse;
-import gg.agit.konect.global.auth.annotation.PublicApi;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -15,6 +14,5 @@ public interface BankApi {
 
     @Operation(summary = "은행 리스트를 조회한다.")
     @GetMapping
-    @PublicApi
     ResponseEntity<BanksResponse> getBanks();
 }
