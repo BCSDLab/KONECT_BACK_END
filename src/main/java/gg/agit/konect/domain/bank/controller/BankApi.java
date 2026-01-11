12877272398a4609baf7b12165556f8b198fe94a
이동훈 <2dh2@naver.com>
2026-01-11 15:17:28 +0900

    feat: 은행 목록 조회 API 명세 작성

---
+++ src/main/java/gg/agit/konect/domain/bank/controller/BankApi.java
@@ -0,0 +1,20 @@
+package gg.agit.konect.domain.bank.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import gg.agit.konect.domain.bank.dto.BanksResponse;
+import gg.agit.konect.global.auth.annotation.PublicApi;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "(Normal) Bank: 은행", description = "은행 API")
+@RequestMapping("/banks")
+public interface BankApi {
+
+    @Operation(summary = "은행 리스트를 조회한다.")
+    @GetMapping
+    @PublicApi
+    ResponseEntity<BanksResponse> getBanks();
+}
