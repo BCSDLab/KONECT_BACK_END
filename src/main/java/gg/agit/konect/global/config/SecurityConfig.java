36bfde10b9def0142a5b92e3c492389cc991f8de
신관규 <soundbar91@gmail.com>
2026-01-13 00:04:01 +0900

    chore: 코드 포멧팅

--- src/main/java/gg/agit/konect/global/config/SecurityConfig.java
+++ src/main/java/gg/agit/konect/global/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
