2d55f7e57adac9cbfa30874004d798db55ddeb2b
이동훈 <2dh2@naver.com>
2026-01-15 14:28:56 +0900

    feat: 애플 로그인 서비스 구현

---
+++ src/main/java/gg/agit/konect/global/auth/oauth/AppleOAuthServiceImpl.java
@@ -0,0 +1,79 @@
+package gg.agit.konect.global.auth.oauth;
+
+import java.util.Optional;
+
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import gg.agit.konect.domain.user.enums.Provider;
+import gg.agit.konect.domain.user.model.UnRegisteredUser;
+import gg.agit.konect.domain.user.model.User;
+import gg.agit.konect.domain.user.repository.UnRegisteredUserRepository;
+import gg.agit.konect.domain.user.repository.UserRepository;
+import gg.agit.konect.global.code.ApiResponseCode;
+import gg.agit.konect.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+
+@Service("apple")
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AppleOAuthServiceImpl implements SocialOAuthService {
+
+    private static final String OIDC_REQUEST_REQUIRED = "invalid_user_request";
+
+    private final UserRepository userRepository;
+    private final UnRegisteredUserRepository unRegisteredUserRepository;
+    private final OidcUserService oidcUserService = new OidcUserService();
+
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        if (!(userRequest instanceof OidcUserRequest oidcUserRequest)) {
+            throw new OAuth2AuthenticationException(new OAuth2Error(OIDC_REQUEST_REQUIRED));
+        }
+
+        OidcUser oidcUser = oidcUserService.loadUser(oidcUserRequest);
+
+        String email = oidcUser.getAttribute("email");
+        String providerId = oidcUser.getSubject();
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
+        Provider provider = Provider.valueOf(registrationId);
+
+        if (userRepository.existsByProviderIdAndProvider(providerId, provider)) {
+            return oidcUser;
+        }
+
+        if (StringUtils.hasText(email)) {
+            Optional<User> registeredByEmail = userRepository.findByEmailAndProvider(email, provider);
+
+            if (registeredByEmail.isPresent()) {
+                return oidcUser;
+            }
+        }
+
+        if (!unRegisteredUserRepository.existsByProviderIdAndProvider(providerId, provider)) {
+            if (!StringUtils.hasText(email)) {
+                throw CustomException.of(ApiResponseCode.FAILED_EXTRACT_EMAIL);
+            }
+
+            UnRegisteredUser newUser = UnRegisteredUser.builder()
+                .email(email)
+                .provider(provider)
+                .providerId(providerId)
+                .build();
+
+            unRegisteredUserRepository.save(newUser);
+        }
+
+        return oidcUser;
+    }
+}
