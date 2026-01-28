d55502f1f2a7a88c8712827744c23070029c84c7
이동훈 <2dh2@naver.com>
2026-01-28 16:59:34 +0900

    refactor: 동아리 태그 삭제

--- src/main/java/gg/agit/konect/domain/club/controller/ClubController.java
+++ src/main/java/gg/agit/konect/domain/club/controller/ClubController.java
@@ -32,7 +32,6 @@ import gg.agit.konect.domain.club.dto.ClubProfileUpdateRequest;
 import gg.agit.konect.domain.club.dto.ClubRecruitmentCreateRequest;
 import gg.agit.konect.domain.club.dto.ClubRecruitmentResponse;
 import gg.agit.konect.domain.club.dto.ClubRecruitmentUpdateRequest;
-import gg.agit.konect.domain.club.dto.ClubTagsResponse;
 import gg.agit.konect.domain.club.dto.ClubsResponse;
 import gg.agit.konect.domain.club.dto.MyManagedClubResponse;
 import gg.agit.konect.domain.club.dto.MemberPositionChangeRequest;
@@ -109,12 +108,6 @@ public class ClubController implements ClubApi {
     ) {
         clubService.updateBasicInfo(clubId, userId, request);
         return ResponseEntity.noContent().build();
-    }
-
-    @Override
-    public ResponseEntity<ClubTagsResponse> getTags() {
-        ClubTagsResponse response = clubService.getTags();
-        return ResponseEntity.ok(response);
     }
 
     @Override
