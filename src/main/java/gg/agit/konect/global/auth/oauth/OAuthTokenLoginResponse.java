f59f70a02b047d5f0dd1bdf73c5781af1a0e6153
이동훈 <2dh2@naver.com>
2026-01-30 22:46:02 +0900

    feat: 스웨거 명세 작성

--- src/main/java/gg/agit/konect/global/auth/oauth/OAuthTokenLoginResponse.java
+++ src/main/java/gg/agit/konect/global/auth/oauth/OAuthTokenLoginResponse.java
@@ -1,8 +1,17 @@
 package gg.agit.konect.global.auth.oauth;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record OAuthTokenLoginResponse(
+    @Schema(description = "리다이렉트 경로", example = "https://agit.gg", requiredMode = REQUIRED)
     String redirectUri,
+
+    @Schema(description = "리프레시 토큰", example = "IQxiK3gPhYIP8...", requiredMode = REQUIRED)
     String refreshToken,
+
+    @Schema(description = "회원가입 토큰", example = "eyJhbGciOiJIUzI1NiJ9...", requiredMode = REQUIRED)
     String signupToken
 ) {
     public static OAuthTokenLoginResponse login(String redirectUri, String refreshToken) {
