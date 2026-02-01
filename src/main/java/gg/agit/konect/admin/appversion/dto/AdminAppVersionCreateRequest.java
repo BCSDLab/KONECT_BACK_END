5894c33cd2155e6582bd77cab9da49ecdf91b4b4
이동훈 <2dh2@naver.com>
2026-02-01 21:54:05 +0900

    feat: 어드민 앱 버전 등록 API 구현

---
+++ src/main/java/gg/agit/konect/admin/appversion/dto/AdminAppVersionCreateRequest.java
@@ -0,0 +1,22 @@
+package gg.agit.konect.admin.appversion.dto;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import gg.agit.konect.domain.appversion.enums.PlatformType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AdminAppVersionCreateRequest(
+    @NotNull(message = "플랫폼은 필수 입력입니다.")
+    @Schema(description = "플랫폼 타입", example = "IOS", requiredMode = REQUIRED)
+    PlatformType platform,
+
+    @NotBlank(message = "버전은 필수 입력입니다.")
+    @Schema(description = "버전", example = "1.2.3", requiredMode = REQUIRED)
+    String version,
+
+    @Schema(description = "릴리즈 노트")
+    String releaseNotes
+) {
+}
