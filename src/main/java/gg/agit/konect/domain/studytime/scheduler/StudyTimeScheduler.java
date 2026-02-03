e985a4442fc1b8f65163f7919b92e2ff5139675a
이동훈 <64298482+dh2906@users.noreply.github.com>
2026-02-03 16:19:22 +0900

    refactor: 스케줄러 로깅 분리 (#169)
    * refactor: 스케줄러 로깅 분리
    
    * feat: 로그 파일 최대 용량 지정

--- src/main/java/gg/agit/konect/domain/studytime/scheduler/StudyTimeScheduler.java
+++ src/main/java/gg/agit/konect/domain/studytime/scheduler/StudyTimeScheduler.java
@@ -2,72 +2,74 @@ package gg.agit.konect.domain.studytime.scheduler;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import gg.agit.konect.domain.studytime.service.StudyTimeSchedulerService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StudyTimeScheduler {
+
+    private static final Logger SCHEDULER_LOGGER = LoggerFactory.getLogger("scheduler.studytime");
 
     private final StudyTimeSchedulerService studyTimeSchedulerService;
 
     @Scheduled(fixedDelay = 5, timeUnit = SECONDS)
     public void updateClubStudyTimeRanking() {
         try {
-            log.info("동아리 공부 시간 랭킹 업데이트 시작");
+            SCHEDULER_LOGGER.info("동아리 공부 시간 랭킹 업데이트 시작");
             studyTimeSchedulerService.updateClubStudyTimeRanking();
-            log.info("동아리 공부 시간 랭킹 업데이트 완료");
+            SCHEDULER_LOGGER.info("동아리 공부 시간 랭킹 업데이트 완료");
         } catch (Exception e) {
-            log.error("동아리 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
+            SCHEDULER_LOGGER.error("동아리 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
         }
     }
 
     @Scheduled(fixedDelay = 5, timeUnit = SECONDS)
     public void updatePersonalStudyTimeRanking() {
         try {
-            log.info("개인 공부 시간 랭킹 업데이트 시작");
+            SCHEDULER_LOGGER.info("개인 공부 시간 랭킹 업데이트 시작");
             studyTimeSchedulerService.updatePersonalStudyTimeRanking();
-            log.info("개인 공부 시간 랭킹 업데이트 완료");
+            SCHEDULER_LOGGER.info("개인 공부 시간 랭킹 업데이트 완료");
         } catch (Exception e) {
-            log.error("개인 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
+            SCHEDULER_LOGGER.error("개인 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
         }
     }
 
     @Scheduled(fixedDelay = 5, timeUnit = SECONDS)
     public void updateStudentNumberStudyTimeRanking() {
         try {
-            log.info("학번별 공부 시간 랭킹 업데이트 시작");
+            SCHEDULER_LOGGER.info("학번별 공부 시간 랭킹 업데이트 시작");
             studyTimeSchedulerService.updateStudentNumberStudyTimeRanking();
-            log.info("학번별 공부 시간 랭킹 업데이트 완료");
+            SCHEDULER_LOGGER.info("학번별 공부 시간 랭킹 업데이트 완료");
         } catch (Exception e) {
-            log.error("학번별 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
+            SCHEDULER_LOGGER.error("학번별 공부 시간 랭킹 업데이트 과정에서 오류가 발생했습니다.", e);
         }
     }
 
     @Scheduled(cron = "0 0 0 * * *")
     public void resetStudyTimeRankingDaily() {
         try {
-            log.info("일일 공부 시간 랭킹 초기화 시작");
+            SCHEDULER_LOGGER.info("일일 공부 시간 랭킹 초기화 시작");
             studyTimeSchedulerService.resetStudyTimeRankingDaily();
-            log.info("일일 공부 시간 랭킹 초기화 완료");
+            SCHEDULER_LOGGER.info("일일 공부 시간 랭킹 초기화 완료");
         } catch (Exception e) {
-            log.error("일일 공부 시간 랭킹 초기화 과정에서 오류가 발생했습니다.", e);
+            SCHEDULER_LOGGER.error("일일 공부 시간 랭킹 초기화 과정에서 오류가 발생했습니다.", e);
         }
     }
 
     @Scheduled(cron = "0 0 0 1 * *")
     public void resetStudyTimeRankingMonthly() {
         try {
-            log.info("월간 공부 시간 랭킹 초기화 시작");
+            SCHEDULER_LOGGER.info("월간 공부 시간 랭킹 초기화 시작");
             studyTimeSchedulerService.resetStudyTimeRankingMonthly();
-            log.info("월간 공부 시간 랭킹 초기화 완료");
+            SCHEDULER_LOGGER.info("월간 공부 시간 랭킹 초기화 완료");
         } catch (Exception e) {
-            log.error("월간 공부 시간 랭킹 초기화 과정에서 오류가 발생했습니다.", e);
+            SCHEDULER_LOGGER.error("월간 공부 시간 랭킹 초기화 과정에서 오류가 발생했습니다.", e);
         }
     }
 }
