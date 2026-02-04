fc86010bfb8ccc6cf862065598b6edb4a28aebcb
이동훈 <2dh2@naver.com>
2026-02-04 21:20:57 +0900

    feat: 디바이스 id, 플랫폼 컬럼 추가

--- src/main/resources/db/migration/V19__add_fcm_device_token_table.sql
+++ src/main/resources/db/migration/V19__add_fcm_device_token_table.sql
@@ -3,9 +3,12 @@ CREATE TABLE IF NOT EXISTS fcm_device_token
     id         INT AUTO_INCREMENT PRIMARY KEY,
     user_id    INT                                                            NOT NULL,
     token      VARCHAR(255)                                                   NOT NULL,
+    device_id  VARCHAR(100)                                                   NOT NULL,
+    platform   VARCHAR(20)                                                    NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP                            NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
 
     CONSTRAINT uq_fcm_device_token_token UNIQUE (token),
+    CONSTRAINT uq_fcm_device_token_user_device UNIQUE (user_id, device_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
