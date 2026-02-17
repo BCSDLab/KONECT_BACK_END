45dcf622aee745f4d76e5c993af036b844ca6abc
이동훈 <2dh2@naver.com>
2026-02-17 21:40:28 +0900

    chore: 맞춤법 개선

--- src/main/java/gg/agit/konect/domain/club/dto/ClubPreMemberAddRequest.java
+++ src/main/java/gg/agit/konect/domain/club/dto/ClubPreMemberAddRequest.java
@@ -17,7 +17,7 @@ public record ClubPreMemberAddRequest(
     String studentNumber,
 
     @NotEmpty(message = "이름은 필수 입력입니다.")
-    @Size(min = 2, max = 5, message = "이름은 2자 이상 5자 이하 입니다.")
+    @Size(min = 2, max = 5, message = "이름은 2자 이상 5자 이하입니다.")
     @Pattern(regexp = "^[가-힣]+$", message = "이름은 완성된 한글만 입력할 수 있습니다.")
     @Schema(description = "사전 등록할 회원 이름", example = "홍길동", requiredMode = REQUIRED)
     String name,
