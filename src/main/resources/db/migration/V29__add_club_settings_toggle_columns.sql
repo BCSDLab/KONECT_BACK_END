021f04f2156993f647492ccdfa4142ffe4b0c740
JanooGwan <janoogwan39@gmail.com>
2026-02-19 03:04:59 +0900

    refactor: DB 마이그레이션 기본값 설정

--- src/main/resources/db/migration/V29__add_club_settings_toggle_columns.sql
+++ src/main/resources/db/migration/V29__add_club_settings_toggle_columns.sql
@@ -1,3 +1,5 @@
--- 동아리 설정 토글 컬럼 추가 (모집공고 활성화, 지원서 활성화)
-ALTER TABLE club ADD COLUMN is_recruitment_enabled TINYINT(1) DEFAULT NULL;
-ALTER TABLE club ADD COLUMN is_application_enabled TINYINT(1) DEFAULT NULL;
+ALTER TABLE club ADD COLUMN is_recruitment_enabled TINYINT(1) NOT NULL DEFAULT 1;
+ALTER TABLE club ADD COLUMN is_application_enabled TINYINT(1) NOT NULL DEFAULT 1;
+
+UPDATE club SET is_recruitment_enabled = 1 WHERE is_recruitment_enabled IS NULL;
+UPDATE club SET is_application_enabled = 1 WHERE is_application_enabled IS NULL;
