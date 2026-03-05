f0e2563a397ecb08010c758e307ff74a83a9c96c
이동훈 <2dh2@naver.com>
2026-03-06 00:28:52 +0900

    docs: OAuthLinkRequest의 access/id token Swagger required 표기를 NOT_REQUIRED로 수정

--- src/main/java/gg/agit/konect/domain/user/dto/OAuthLinkRequest.java
+++ src/main/java/gg/agit/konect/domain/user/dto/OAuthLinkRequest.java
@@ -1,5 +1,6 @@
 package gg.agit.konect.domain.user.dto;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,10 +11,18 @@ public record OAuthLinkRequest(
     @Schema(description = "OAuth 제공자", example = "google", requiredMode = REQUIRED)
     String provider,
 
-    @Schema(description = "OAuth access token", example = "eyJhbGciOi...", requiredMode = REQUIRED)
+    @Schema(
+        description = "OAuth access token (provider별로 필요 여부가 다름)",
+        example = "eyJhbGciOi...",
+        requiredMode = NOT_REQUIRED
+    )
     String accessToken,
 
-    @Schema(description = "OAuth id token", example = "eyJraWQiOi...", requiredMode = REQUIRED)
+    @Schema(
+        description = "OAuth id token (provider별로 필요 여부가 다름)",
+        example = "eyJraWQiOi...",
+        requiredMode = NOT_REQUIRED
+    )
     String idToken,
 
     @Schema(description = "OAuth 제공자에서 전달된 사용자 이름", example = "홍길동")
