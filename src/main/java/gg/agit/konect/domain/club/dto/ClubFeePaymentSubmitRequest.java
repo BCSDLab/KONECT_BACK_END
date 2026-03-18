dec6d8afa810af6b4dd2c8a637fe2670fe708edc
JanooGwan <janoogwan39@gmail.com>
2026-03-18 11:56:58 +0900

    fix: FeePayment 파일 삭제 누락 수정 (git rm)

--- src/main/java/gg/agit/konect/domain/club/dto/ClubFeePaymentSubmitRequest.java
+++
@@ -1,9 +0,0 @@
-package gg.agit.konect.domain.club.dto;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record ClubFeePaymentSubmitRequest(
-    @Schema(description = "Payment image URL", example = "https://cdn.konect.com/fee/abc.jpg")
-    String paymentImageUrl
-) {
-}
--- src/main/java/gg/agit/konect/domain/club/event/ClubFeePaymentApprovedEvent.java
+++
@@ -1,9 +0,0 @@
-package gg.agit.konect.domain.club.event;
-
-public record ClubFeePaymentApprovedEvent(
-    Integer clubId
-) {
-    public static ClubFeePaymentApprovedEvent of(Integer clubId) {
-        return new ClubFeePaymentApprovedEvent(clubId);
-    }
-}
