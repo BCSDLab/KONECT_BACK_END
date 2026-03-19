8ef27643b666421804683a5e3373f4d670499b95
이동훈 <64298482+dh2906@users.noreply.github.com>
2026-03-20 00:38:16 +0900

    Revert "hotxfix: 채팅방 목록 조회 응답 속도 개선 (develop) (#418)"
    This reverts commit 29e06b69c41906f4addb8870b4f44540d318e589.

--- src/main/java/gg/agit/konect/domain/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/gg/agit/konect/domain/chat/repository/ChatRoomMemberRepository.java
@@ -68,17 +68,6 @@ public interface ChatRoomMemberRepository extends Repository<ChatRoomMember, Cha
         """)
     List<ChatRoomMember> findByUserId(@Param("userId") Integer userId);
 
-    @Query("""
-        SELECT crm
-        FROM ChatRoomMember crm
-        WHERE crm.id.chatRoomId IN :chatRoomIds
-          AND crm.id.userId = :userId
-        """)
-    List<ChatRoomMember> findByChatRoomIdsAndUserId(
-        @Param("chatRoomIds") List<Integer> chatRoomIds,
-        @Param("userId") Integer userId
-    );
-
     @Modifying
     @Query("""
         UPDATE ChatRoomMember crm
