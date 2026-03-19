7af662aed21783192c8de75d4866c4782d39e169
이동훈 <2dh2@naver.com>
2026-03-19 14:01:55 +0900

    chore: 코드 포맷팅

--- src/test/java/gg/agit/konect/integration/domain/club/ClubSettingsControllerTest.java
+++ src/test/java/gg/agit/konect/integration/domain/club/ClubSettingsControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.jdbc.Sql;
 
 import gg.agit.konect.domain.club.dto.ClubSettingsUpdateRequest;
 import gg.agit.konect.domain.club.model.Club;
