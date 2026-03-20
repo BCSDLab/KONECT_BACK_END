3bb784bf65013874cb881be0d0804afae136b108
JanooGwan <janoogwan39@gmail.com>
2026-03-20 17:45:58 +0900

    fix: NOT_FOUND_ADVERTISEMENT 누락 복구 - develop 브랜치 충돌 해결

--- src/main/java/gg/agit/konect/global/code/ApiResponseCode.java
+++ src/main/java/gg/agit/konect/global/code/ApiResponseCode.java
@@ -91,6 +91,7 @@ public enum ApiResponseCode {
     NOT_FOUND_BANK(HttpStatus.NOT_FOUND, "해당하는 은행을 찾을 수 없습니다."),
     NOT_FOUND_VERSION(HttpStatus.NOT_FOUND, "버전을 찾을 수 없습니다."),
     NOT_FOUND_NOTIFICATION_TOKEN(HttpStatus.NOT_FOUND, "알림 토큰을 찾을 수 없습니다."),
+    NOT_FOUND_ADVERTISEMENT(HttpStatus.NOT_FOUND, "광고를 찾을 수 없습니다."),
     NOT_FOUND_CLUB_SHEET_ID(HttpStatus.NOT_FOUND, "등록된 스프레드시트 ID가 없습니다. 먼저 스프레드시트 ID를 등록해 주세요."),
 
     // 405 Method Not Allowed (지원하지 않는 HTTP 메소드)
