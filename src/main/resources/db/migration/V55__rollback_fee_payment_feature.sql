5fc337f56da185ab5173a2f8e8df1693307e6181
JanooGwan <janoogwan39@gmail.com>
2026-03-20 17:08:48 +0900

    chore: ClubFeePayment 기능 롤백 마이그레이션 추가

--- src/main/resources/db/migration/V55__rollback_fee_payment_feature.sql
+++ src/main/resources/db/migration/V55__rollback_fee_payment_feature.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS club_fee_payment;
 
 -- V53 rollback: drop fee_sheet columns from club
 ALTER TABLE club
-    DROP COLUMN IF EXISTS fee_sheet_id,
-    DROP COLUMN IF EXISTS fee_sheet_column_mapping;
+    DROP COLUMN fee_sheet_id;
+
+ALTER TABLE club
+    DROP COLUMN fee_sheet_column_mapping;
