5e0c6c2aaf476fd100b21a30bd5ad45e0ba0ccf6
이동훈 <2dh2@naver.com>
2026-03-22 18:39:14 +0900

    chore: 코드 포맷팅

--- src/main/java/gg/agit/konect/domain/chat/service/ChatService.java
+++ src/main/java/gg/agit/konect/domain/chat/service/ChatService.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
