b860eec05a839b3a4f3cefe988b9ca9f67942a90
JanooGwan <janoogwan39@gmail.com>
2026-03-24 11:56:32 +0900

    fix sync: replace tick-prefix phone with PhoneNumberNormalizer.format()

--- src/main/java/gg/agit/konect/global/util/PhoneNumberNormalizer.java
+++ src/main/java/gg/agit/konect/global/util/PhoneNumberNormalizer.java
@@ -5,6 +5,16 @@ public final class PhoneNumberNormalizer {
     private static final int PHONE_NUMBER_MIN_DIGITS = 9;
     private static final int PHONE_NUMBER_MAX_DIGITS = 11;
 
+    private static final int DIGITS_9 = 9;
+    private static final int DIGITS_10 = 10;
+    private static final int DIGITS_11 = 11;
+
+    private static final int IDX_2 = 2;
+    private static final int IDX_3 = 3;
+    private static final int IDX_5 = 5;
+    private static final int IDX_6 = 6;
+    private static final int IDX_7 = 7;
+
     private PhoneNumberNormalizer() {
     }
 
@@ -42,33 +52,43 @@ public final class PhoneNumberNormalizer {
         }
 
         // 구글 시트에서 앞자리 0이 잘린 경우 복구: 10자리이고 1로 시작
-        if (digits.length() == 10 && digits.startsWith("1")) {
+        if (digits.length() == DIGITS_10 && digits.startsWith("1")) {
             digits = "0" + digits;
         }
 
-        // 011자리: XXX-XXXX-XXXX
-        if (digits.length() == 11) {
-            return digits.substring(0, 3) + "-" + digits.substring(3, 7) + "-" + digits.substring(7);
+        // 11자리: XXX-XXXX-XXXX
+        if (digits.length() == DIGITS_11) {
+            return digits.substring(0, IDX_3)
+                + "-" + digits.substring(IDX_3, IDX_7)
+                + "-" + digits.substring(IDX_7);
         }
 
-        // 010자리(10자리 010 시작): XXX-XXX-XXXX
-        if (digits.length() == 10 && digits.startsWith("010")) {
-            return digits.substring(0, 3) + "-" + digits.substring(3, 6) + "-" + digits.substring(6);
+        // 010으로 시작하는 10자리: XXX-XXX-XXXX
+        if (digits.length() == DIGITS_10 && digits.startsWith("010")) {
+            return digits.substring(0, IDX_3)
+                + "-" + digits.substring(IDX_3, IDX_6)
+                + "-" + digits.substring(IDX_6);
         }
 
         // 서울 지역번호 02: 02-XXXX-XXXX(10자리) or 02-XXX-XXXX(9자리)
         if (digits.startsWith("02")) {
-            if (digits.length() == 10) {
-                return digits.substring(0, 2) + "-" + digits.substring(2, 6) + "-" + digits.substring(6);
+            if (digits.length() == DIGITS_10) {
+                return digits.substring(0, IDX_2)
+                    + "-" + digits.substring(IDX_2, IDX_6)
+                    + "-" + digits.substring(IDX_6);
             }
-            if (digits.length() == 9) {
-                return digits.substring(0, 2) + "-" + digits.substring(2, 5) + "-" + digits.substring(5);
+            if (digits.length() == DIGITS_9) {
+                return digits.substring(0, IDX_2)
+                    + "-" + digits.substring(IDX_2, IDX_5)
+                    + "-" + digits.substring(IDX_5);
             }
         }
 
         // 기타 10자리: XXX-XXX-XXXX
-        if (digits.length() == 10) {
-            return digits.substring(0, 3) + "-" + digits.substring(3, 6) + "-" + digits.substring(6);
+        if (digits.length() == DIGITS_10) {
+            return digits.substring(0, IDX_3)
+                + "-" + digits.substring(IDX_3, IDX_6)
+                + "-" + digits.substring(IDX_6);
         }
 
         return digits;
