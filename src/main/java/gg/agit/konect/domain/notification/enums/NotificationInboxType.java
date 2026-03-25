e4d1915b0f252e93b554395c6b87720440812355
이동훈 <64298482+dh2906@users.noreply.github.com>
2026-03-25 18:50:48 +0900

    fix: 인앱 알림 SSE 연결 문제 해결 (#442)
    * feat: 채팅 메시지에 대한 인앱 알림도 추가
    
    * test: 테스트 실패하는 문제 해결
    
    * fix: SSE 요청 로깅에서 제외
    
    * feat: 읽지 않은 쪽지에 대한 알림 타입 추가

--- src/main/java/gg/agit/konect/domain/notification/enums/NotificationInboxType.java
+++ src/main/java/gg/agit/konect/domain/notification/enums/NotificationInboxType.java
@@ -3,5 +3,8 @@ package gg.agit.konect.domain.notification.enums;
 public enum NotificationInboxType {
     CLUB_APPLICATION_SUBMITTED,
     CLUB_APPLICATION_APPROVED,
-    CLUB_APPLICATION_REJECTED
+    CLUB_APPLICATION_REJECTED,
+    CHAT_MESSAGE,
+    GROUP_CHAT_MESSAGE,
+    UNREAD_CHAT_COUNT
 }
