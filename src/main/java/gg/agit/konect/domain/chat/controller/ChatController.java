b126b48352786ccbb54b3bf4af9883e105b945e4
이동훈 <2dh2@naver.com>
2026-03-31 22:21:07 +0900

    chore: 코드 포맷팅

--- src/main/java/gg/agit/konect/domain/chat/controller/ChatController.java
+++ src/main/java/gg/agit/konect/domain/chat/controller/ChatController.java
@@ -3,7 +3,6 @@ package gg.agit.konect.domain.chat.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
