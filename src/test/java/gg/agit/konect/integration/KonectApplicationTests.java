e6ee8e5045f9ceba20bec70385ef2c3a57957fa0
이동훈 <64298482+dh2906@users.noreply.github.com>
2026-03-31 20:42:04 +0900

    fix: Google 인증 파일 경로 classpath 처리 추가 (#469)

--- src/test/java/gg/agit/konect/integration/KonectApplicationTests.java
+++ src/test/java/gg/agit/konect/integration/KonectApplicationTests.java
@@ -5,19 +5,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.google.api.services.drive.Drive;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.auth.oauth2.GoogleCredentials;
 
 import gg.agit.konect.support.TestClaudeConfig;
+import gg.agit.konect.support.TestMcpConfig;
 import gg.agit.konect.support.TestSecurityConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import({TestClaudeConfig.class, TestSecurityConfig.class})
+@Import({TestClaudeConfig.class, TestMcpConfig.class, TestSecurityConfig.class})
 @TestPropertySource(locations = "classpath:.env.test.properties")
 class KonectApplicationTests {
+
+    @MockitoBean
+    private GoogleCredentials googleCredentials;
+
+    @MockitoBean
+    private Sheets googleSheetsService;
+
+    @MockitoBean
+    private Drive googleDriveService;
 
     @Test
     void contextLoads() {
     }
 
 }
-
