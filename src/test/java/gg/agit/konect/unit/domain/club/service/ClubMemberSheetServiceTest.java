166e7210533ca80418e4ca1ed34c57593c93f2ac
이동훈 <2dh2@naver.com>
2026-04-10 14:45:27 +0900

    test: ClubMemberSheetService updateSheetId/누락 sheetId 엣지 케이스를 보강
    - updateSheetId 정상 동작
    - syncMembersToSheet sheetId null/blank 시 NOT_FOUND_CLUB_SHEET_ID 예외

--- src/test/java/gg/agit/konect/unit/domain/club/service/ClubMemberSheetServiceTest.java
+++ src/test/java/gg/agit/konect/unit/domain/club/service/ClubMemberSheetServiceTest.java
@@ -1,6 +1,8 @@
 package gg.agit.konect.unit.domain.club.service;
 
+import static gg.agit.konect.global.code.ApiResponseCode.NOT_FOUND_CLUB_SHEET_ID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -9,9 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gg.agit.konect.domain.club.dto.ClubMemberSheetSyncResponse;
+import gg.agit.konect.domain.club.dto.ClubSheetIdUpdateRequest;
 import gg.agit.konect.domain.club.enums.ClubSheetSortKey;
 import gg.agit.konect.domain.club.model.Club;
 import gg.agit.konect.domain.club.repository.ClubMemberRepository;
@@ -21,6 +25,7 @@ import gg.agit.konect.domain.club.service.ClubMemberSheetService;
 import gg.agit.konect.domain.club.service.ClubPermissionValidator;
 import gg.agit.konect.domain.club.service.SheetHeaderMapper;
 import gg.agit.konect.domain.club.service.SheetSyncExecutor;
+import gg.agit.konect.global.exception.CustomException;
 import gg.agit.konect.support.ServiceTestSupport;
 import gg.agit.konect.support.fixture.ClubFixture;
 import gg.agit.konect.support.fixture.UniversityFixture;
@@ -79,5 +84,77 @@ class ClubMemberSheetServiceTest extends ServiceTestSupport {
         assertThat(response.syncedMemberCount()).isEqualTo(5);
         assertThat(response.sheetUrl())
             .isEqualTo("https://docs.google.com/spreadsheets/d/" + spreadsheetId + "/edit");
+    }
+
+    @Test
+    @DisplayName("updateSheetId는 정상 동작한다")
+    void updateSheetIdWorksNormally() throws JsonProcessingException {
+        // given
+        Integer clubId = 1;
+        Integer requesterId = 2;
+        String spreadsheetUrl = "https://docs.google.com/spreadsheets/d/test-sheet-id/edit";
+        Club club = ClubFixture.create(UniversityFixture.create());
+        ClubSheetIdUpdateRequest request = new ClubSheetIdUpdateRequest(spreadsheetUrl);
+        gg.agit.konect.domain.club.model.SheetColumnMapping mapping = gg.agit.konect.domain.club.model.SheetColumnMapping.defaultMapping();
+        SheetHeaderMapper.SheetAnalysisResult analysisResult = new SheetHeaderMapper.SheetAnalysisResult(
+            mapping,
+            null,
+            null
+        );
+
+        given(clubRepository.getById(clubId)).willReturn(club);
+        given(sheetHeaderMapper.analyzeAllSheets("test-sheet-id")).willReturn(analysisResult);
+        given(objectMapper.writeValueAsString(analysisResult.memberListMapping().toMap())).willReturn("{}");
+
+        // when
+        clubMemberSheetService.updateSheetId(clubId, requesterId, request);
+
+        // then
+        verify(clubPermissionValidator).validateManagerAccess(clubId, requesterId);
+        verify(sheetHeaderMapper).analyzeAllSheets("test-sheet-id");
+        assertThat(club.getGoogleSheetId()).isEqualTo("test-sheet-id");
+    }
+
+    @Test
+    @DisplayName("syncMembersToSheet는 sheetId가 null인 경우 NOT_FOUND_CLUB_SHEET_ID 예외를 던진다")
+    void syncMembersToSheetThrowsNotFoundClubSheetIdWhenSheetIdIsNull() {
+        // given
+        Integer clubId = 1;
+        Integer requesterId = 2;
+        Club club = ClubFixture.create(UniversityFixture.create());
+
+        given(clubRepository.getById(clubId)).willReturn(club);
+
+        // when & then
+        assertThatThrownBy(() -> clubMemberSheetService.syncMembersToSheet(
+            clubId,
+            requesterId,
+            ClubSheetSortKey.POSITION,
+            true
+        ))
+            .isInstanceOf(CustomException.class)
+            .satisfies(exception -> assertThat(((CustomException) exception).getErrorCode()).isEqualTo(NOT_FOUND_CLUB_SHEET_ID));
+    }
+
+    @Test
+    @DisplayName("syncMembersToSheet는 sheetId가 blank인 경우 NOT_FOUND_CLUB_SHEET_ID 예외를 던진다")
+    void syncMembersToSheetThrowsNotFoundClubSheetIdWhenSheetIdIsBlank() {
+        // given
+        Integer clubId = 1;
+        Integer requesterId = 2;
+        Club club = ClubFixture.create(UniversityFixture.create());
+        club.updateGoogleSheetId("   ");
+
+        given(clubRepository.getById(clubId)).willReturn(club);
+
+        // when & then
+        assertThatThrownBy(() -> clubMemberSheetService.syncMembersToSheet(
+            clubId,
+            requesterId,
+            ClubSheetSortKey.POSITION,
+            true
+        ))
+            .isInstanceOf(CustomException.class)
+            .satisfies(exception -> assertThat(((CustomException) exception).getErrorCode()).isEqualTo(NOT_FOUND_CLUB_SHEET_ID));
     }
 }
