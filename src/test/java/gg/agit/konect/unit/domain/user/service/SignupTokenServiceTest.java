258847a179b0661985034c989b18e54ac194f898
이동훈 <2dh2@naver.com>
2026-04-12 17:00:33 +0900

    chore: 코드 포맷팅

--- src/test/java/gg/agit/konect/unit/domain/user/service/SignupTokenServiceTest.java
+++ src/test/java/gg/agit/konect/unit/domain/user/service/SignupTokenServiceTest.java
@@ -2,7 +2,6 @@ package gg.agit.konect.unit.domain.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
