8fe3ee5bd26a0b26a70e376af5cd531e91c83157
이동훈 <2dh2@naver.com>
2026-05-18 14:00:10 +0900

    fix: 대학 이미지 기본값을 샘플 로고로 지정
    - 기존 대학 데이터의 image_url 백필값을 운영 가능한 샘플 로고 URL로 지정
    - 테스트 fixture 기본값도 같은 URL을 사용해 필수 이미지 계약과 테스트 데이터가 일치하도록 정리

--- src/main/resources/db/migration/V73__add_image_url_to_university.sql
+++ src/main/resources/db/migration/V73__add_image_url_to_university.sql
@@ -2,7 +2,7 @@ ALTER TABLE university
     ADD COLUMN image_url VARCHAR(255) NULL AFTER region;
 
 UPDATE university
-SET image_url = ''
+SET image_url = 'https://stage-static.koreatech.in/konect/user/university_logo_sample.png'
 WHERE image_url IS NULL;
 
 ALTER TABLE university
