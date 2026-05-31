89ba1635102022acb8d0753fa239c8f26d999e85
JanooGwan <janoogwan39@gmail.com>
2026-06-01 01:15:30 +0900

    test: 대학 검색 키워드 시드 검증 추가

--- src/test/java/gg/agit/konect/support/fixture/UniversitySearchKeywordFixture.java
+++ src/test/java/gg/agit/konect/support/fixture/UniversitySearchKeywordFixture.java
@@ -1,5 +1,7 @@
 package gg.agit.konect.support.fixture;
 
+import java.util.Locale;
+
 import gg.agit.konect.domain.university.enums.UniversitySearchKeywordType;
 import gg.agit.konect.domain.university.model.University;
 import gg.agit.konect.domain.university.model.UniversitySearchKeyword;
@@ -10,7 +12,7 @@ public class UniversitySearchKeywordFixture {
         return UniversitySearchKeyword.builder()
             .university(university)
             .keyword(keyword)
-            .normalizedKeyword(keyword.replaceAll("\\s", "").toLowerCase())
+            .normalizedKeyword(keyword.replaceAll("\\s", "").toLowerCase(Locale.ROOT))
             .keywordType(UniversitySearchKeywordType.ALIAS)
             .build();
     }
